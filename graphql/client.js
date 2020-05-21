0d39bb3a107939bbd8b3158ba786ee4245485c3d
Razvan Mirleneanu <razvan.mirleneanu@lola.tech>
2020-05-21 20:31:24 +0300

    feat: use environment variables

--- graphql/client.js
+++ graphql/client.js
@@ -3,12 +3,9 @@ import ApolloClient from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
-const URI = "https://openapi.radiofrance.fr/v1/graphql?x-token=";
-const TOKEN = "1e17d8b8-afaa-4ba2-ba5c-dddf695fa642";
-
 export default new ApolloClient({
   link: createHttpLink({
-    uri: `${URI}${TOKEN}`,
+    uri: `${URI}${process.env.x_token}`,
     headers: {
       "x-token": TOKEN,
     },
