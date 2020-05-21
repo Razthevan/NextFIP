df4f48d467a2e1a12fd4d5821286ba6d15d3ee2d
Razvan Mirleneanu <razvan.mirleneanu@lola.tech>
2020-05-21 20:32:59 +0300

    fix: redefine URI variable

--- graphql/client.js
+++ graphql/client.js
@@ -3,11 +3,13 @@ import ApolloClient from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
+const URI = "https://openapi.radiofrance.fr/v1/graphql?x-token=";
+
 export default new ApolloClient({
   link: createHttpLink({
     uri: `${URI}${process.env.x_token}`,
     headers: {
-      "x-token": TOKEN,
+      "x-token": process.env.x_token,
     },
   }),
   cache: new InMemoryCache(),
