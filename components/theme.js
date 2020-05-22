297803a9f03c18da9dbdb7408e1cbfd41ab92287
Razvan Mirleneanu <razvan.mirleneanu@lola.tech>
2020-05-22 19:52:10 +0300

    feat: add eslint and webradio images

--- components/theme.js
+++ components/theme.js
@@ -1,12 +1,34 @@
+const fipRock = "FIP_ROCK";
+const fipElectro = "FIP_ELECTRO";
+const fipGroove = "FIP_GROOVE";
+const fipJazz = "FIP_JAZZ";
+const fipMetal = "FIP_METAL";
+const fipNouveautes = "FIP_NOUVEAUTES";
+const fipReggae = "FIP_REGGAE";
+const fipWorld = "FIP_WORLD";
+
+const webRadiosColors = {
+  [fipRock]: "#f93446",
+  [fipElectro]: "#00D3FF",
+  [fipGroove]: "#8664EE",
+  [fipJazz]: "#13898d",
+  [fipMetal]: "#f93446",
+  [fipNouveautes]: "#357ded",
+  [fipReggae]: "#477442",
+  [fipWorld]: "#EFA439",
+};
+
 export const lightTheme = {
   body: "#FFF",
   text: "#363537",
   toggleBorder: "#FFF",
   background: "#363537",
+  ...webRadiosColors,
 };
 export const darkTheme = {
   body: "#363537",
   text: "#FAFAFA",
   toggleBorder: "#6B8096",
   background: "#999",
+  ...webRadiosColors,
 };
--- pageComponents/indexPage/fipRadio/WebRadios.js
+++ pageComponents/indexPage/fipRadio/WebRadios.js
@@ -1,12 +1,20 @@
+import PropTypes from "prop-types";
 import styled from "styled-components";
+
+const propTypes = {
+  webRadios: PropTypes.array,
+};
 
 const WebRadios = ({ webRadios }) => {
   return (
     <Grid>
       {webRadios.map((webRadio) => (
         <Card key={webRadio.id}>
-          <h2>{webRadio.title}</h2>
-          <p>{webRadio.description}</p>
+          <div>
+            <WebRadioTitle id={webRadio.id}>{webRadio.title}</WebRadioTitle>
+            <p>{webRadio.description}</p>
+          </div>
+          <WebRadioLogo src={`${webRadio.id}.jpg`} alt={webRadio.title} />
         </Card>
       ))}
     </Grid>
@@ -16,39 +24,41 @@ const WebRadios = ({ webRadios }) => {
 const Card = styled.div`
   margin: 1rem;
   flex-basis: 45%;
-  padding: 1.5rem;
+  padding: 1rem;
   text-align: left;
   color: inherit;
   text-decoration: none;
-  border: 1px solid #eaeaea;
-  border-radius: 10px;
   transition: color 0.15s ease, border-color 0.15s ease;
   cursor: default;
-  min-height: 20vh;
   display: flex;
   flex-direction: column;
-  align-items: end;
+  justify-content: space-between;
+  min-height: 30vh;
+`;
 
-  &:hover,
-  &:focus,
-  &:active {
-    color: #a90042;
-    border-color: #a90042;
-  }
+const WebRadioTitle = styled.h2`
+  color: ${(props) => props.theme[props.id]};
+`;
+
+const WebRadioLogo = styled.img`
+  max-width: 200px;
 `;
 
 const Grid = styled.div`
   display: flex;
-  align-items: center;
+  align-items: baseline;
   justify-content: center;
   flex-wrap: wrap;
-  max-width: 800px;
+  max-width: 900px;
   margin-top: 3rem;
+  flex-direction: row;
 
   @media (max-width: 600px) {
     width: 100%;
     flex-direction: column;
   }
 `;
+
+WebRadios.propTypes = propTypes;
 
 export default WebRadios;
